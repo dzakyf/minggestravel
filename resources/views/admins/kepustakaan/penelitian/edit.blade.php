397d9a33e7196654646b2c58fa0c70ab7895cc1a
fanyfernaldi <fanyfernaldi@gmail.com>
2020-01-27 11:39:17 +0700

    menambahkan folder kepustakaan di public view

---
+++ resources/views/admins/kepustakaan/penelitian/edit.blade.php
@@ -0,0 +1,142 @@
+@extends('layout.admin')
+
+@section('css')
+<!-- summernote -->
+<link rel="stylesheet" href="{{URL::asset('extadmin/plugins/summernote/summernote-bs4.css')}}">
+@endsection
+
+@section('content')
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Edit Penelitian</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{ url('/admin') }}">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ url('/admin/kepustakaan/penelitian') }}">Perpustakaan</a></li>
+              <li class="breadcrumb-item active">Edit Penelitian</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- SELECT2 EXAMPLE -->
+        <div class="card card-default">
+          <div class="card-header">
+            <h3 class="card-title">Form Edit Penelitian</h3>
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <form method="post" action="{{ url('/admin/kepustakaan/penelitian') }}/{{$penelitian->id_penelitian}}" enctype="multipart/form-data">
+          @method('patch')
+          @csrf
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="id_penelitian">Id Penelitian</label>
+                    <input type="text" class="form-control" style="width: 100%;" disabled="disabled" name="id_penelitian" id="id_penelitian" value="{{$penelitian->id_penelitian}}">
+                  </div>
+                  <!-- /.form-group -->
+                </div>
+                <!-- /.col -->
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="judul">Judul</label>
+                    <input type="text" class="form-control @error('judul') is-invalid @enderror" style="width: 100%;" name="judul" id="judul" placeholder="Masukkan Judul" value="{{$penelitian->judul}}">
+                    <!-- munculin pesan error jika salah -->
+                    @error('judul')
+                            <div class="invalid-feedback"> {{ $message }} </div>
+                    @enderror
+                  </div>
+                  <!-- /.form-group -->
+                </div>
+                <!-- /.col -->
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="peneliti">Peneliti</label>
+                    <input type="text" class="form-control @error('peneliti') is-invalid @enderror" style="width: 100%;" name="peneliti" id="peneliti" placeholder="Masukkan Peneliti" value="{{$penelitian->peneliti}}">
+                    <!-- munculin pesan error jika salah -->
+                    @error('peneliti')
+                            <div class="invalid-feedback"> {{ $message }} </div>
+                    @enderror
+                  </div>
+                  <!-- /.form-group -->
+                </div>
+                <!-- /.col -->
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="tanggal">Tanggal Penelitian</label>
+                    <input type="text" class="form-control @error('tanggal') is-invalid @enderror" style="width: 100%;" name="tanggal" id="tanggal" placeholder="Masukkan Tanggal Penelitian" value="{{$penelitian->tanggal}}">
+                    <!-- munculin pesan error jika salah -->
+                    @error('tanggal')
+                            <div class="invalid-feedback"> {{ $message }} </div>
+                    @enderror
+                  </div>
+                  <!-- /.form-group -->
+                </div>
+                <!-- /.col -->
+
+                <div class="col-md-12">
+                  <div class="form-group">
+                      <label for="deskripsi">Deskripsi</label>
+                      <textarea name="deskripsi" id="deskripsi" class="textarea form-control @error('deskripsi') is-invalid @enderror" placeholder="Masukkan Deskripsi" 
+                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
+                            {{$penelitian->deskripsi}}
+                      </textarea>
+                      <!-- munculin pesan error jika salah -->
+                      @error('deskripsi')
+                            <div class="invalid-feedback"> {{ $message }} </div>
+                      @enderror
+                  </div>
+                  <!-- /.form group -->
+                </div>
+                <!-- /.col -->
+               
+                <div class="col-12 text-center">
+                  <div class="form-group">
+                    <input class="btn btn-primary" type="submit" value="Update">
+                  </div>
+                  <!-- /.form-group -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+            <!-- /.card-body -->
+          </form>
+          <!-- /form -->
+        </div>
+        <!-- /.card -->
+
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  @section('script')
+    <script src="{{URL::asset('extadmin/plugins/summernote/summernote-bs4.min.js')}}"></script>
+    <script>
+    $(function () {
+        // Summernote
+        $('.textarea').summernote()
+    })
+    </script>
+  @endsection
---
+++ resources/views/admins/kepustakaan/penelitian/show.blade.php
@@ -0,0 +1,150 @@
+@extends('layout.admin')
+
+@section('css')
+<!-- summernote -->
+<link rel="stylesheet" href="{{URL::asset('extadmin/plugins/summernote/summernote-bs4.css')}}">
+@endsection
+
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Detail Penelitian</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{ url('/admin') }}">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ url('/admin/kepustakaan/penelitian') }}">Perpustakaan</a></li>
+              <li class="breadcrumb-item active">Detail Penelitian</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- SELECT2 EXAMPLE -->
+        <div class="card card-default">
+          <div class="card-header">
+            <h3 class="card-title">Detail Penelitian</h3>
+
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <div class="row">
+
+              <!-- event id : -->
+              <div class="col-md-3">
+              <p class="card-title">Id Penelitian</p> <p class="text-right">:</p> 
+              </div>
+              <!-- /.col -->
+              <div class="col-md-9">
+                <p class="card-text">{{$penelitian->id_penelitian}}</p>
+              </div>
+              <!-- /.col -->
+
+              <!-- nama_event -->
+              <div class="col-md-3">
+                <p class="card-title">Judul</p><p class="text-right">:</p>  
+              </div>
+              <!-- /.col -->
+              <div class="col-md-9">
+                <p class="card-text">{{$penelitian->judul}}</p> 
+              </div>
+              <!-- /.col -->
+
+              <!-- nama_event -->
+              <div class="col-md-3">
+                <p class="card-title">Peneliti</p><p class="text-right">:</p>  
+              </div>
+              <!-- /.col -->
+              <div class="col-md-9">
+                <p class="card-text">{{$penelitian->peneliti}}</p> 
+              </div>
+              <!-- /.col -->
+
+              <!-- nama_event -->
+              <div class="col-md-3">
+                <p class="card-title">Tanggal Penelitian</p><p class="text-right">:</p>  
+              </div>
+              <!-- /.col -->
+              <div class="col-md-9">
+                <p class="card-text">{{$penelitian->tanggal}}</p> 
+              </div>
+              <!-- /.col -->
+
+              <!-- Deskripsi Singkat -->
+              <div class="col-md-3">
+                <p class="card-title">Deskripsi</p><p class="text-right">:</p>  
+              </div>
+              <!-- /.col -->
+              <div class="col-md-12">
+                <textarea name="deskripsi" id="deskripsi" class="textarea1" placeholder="Masukkan Reseinsi" value=""
+                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">{{$penelitian->deskripsi}}</textarea>
+              </div>
+              <!-- /.col -->
+
+              <!-- Created at -->
+              <div class="col-md-3">
+                <p class="card-title">created At</p><p class="text-right">:</p>  
+              </div>
+              <!-- /.col -->
+              <div class="col-md-9">
+                <p class="card-text">{{$penelitian->created_at}}</p> 
+              </div>
+              <!-- /.col -->
+
+              <!-- Updated at -->
+              <div class="col-md-3">
+                <p class="card-title">Updated At</p><p class="text-right">:</p>  
+              </div>
+              <!-- /.col -->
+              <div class="col-md-9">
+                <p class="card-text">{{$penelitian->updated_at}}</p> 
+              </div>
+              <!-- /.col -->
+
+              <div class="col mb-2 text-center">
+                <a href="{{ $penelitian->id_penelitian }}/edit" class="btn btn-primary">Edit</a>
+
+                <form action="{{ url('/admin/kepustakaan/penelitian')}}/{{ $penelitian->id_penelitian }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Hapus</button>
+                </form>
+              </div>
+
+            <!-- /.row -->
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+        
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  @endsection
+
+  @section('script')
+    <script src="{{URL::asset('extadmin/plugins/summernote/summernote-bs4.min.js')}}"></script>
+    <script>
+    $(function () {
+        // Summernote
+        $('.textarea1').summernote("disable")
+        $('.textarea2').summernote("disable")
+    })
+    </script>
+  @endsection
