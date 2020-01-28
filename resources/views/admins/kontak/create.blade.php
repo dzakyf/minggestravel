ff0ba88d85b603d4faf17bca350febb632a1ab0e
fanyfernaldi <fanyfernaldi@gmail.com>
2020-01-28 15:50:12 +0700

    CRUD kontak, membuat file baru

---
+++ resources/views/admins/kontak/create.blade.php
@@ -0,0 +1,93 @@
+@extends('layout.admin')
+
+@section('css')
+<!-- summernote -->
+<link rel="stylesheet" href="{{URL::asset('extadmin/plugins/summernote/summernote-bs4.css')}}">
+@endsection
+
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Tambah Kontak</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{ url('/admin') }}">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ url('/admin/kontak') }}">Kontak</a></li>
+              <li class="breadcrumb-item active">Tambah Kontak</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- SELECT2 EXAMPLE -->
+        <div class="card card-default">
+          <div class="card-header">
+            <h3 class="card-title">Form Tambah Kontak</h3>
+
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <form method="post" action="{{ url('/admin/kontak') }}" enctype="multipart/form-data">
+          @csrf
+            <div class="card-body">
+              <div class="row">
+                
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" style="width: 100%;" name="email" id="email" placeholder="Masukkan Email" value="{{old('email')}}">
+                    <!-- munculin pesan error jika salah -->
+                    @error('email')
+                            <div class="invalid-feedback"> {{ $message }} </div>
+                    @enderror
+                  </div>
+                  <!-- /.form-group -->
+                </div>
+                <!-- /.col -->
+
+                <div class="col-12">
+                  <div class="form-group">
+                    <input class="btn btn-primary" type="submit" value="Tambah">
+                  </div>
+                  <!-- /.form-group -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+            <!-- /.card-body -->
+          </form>
+          <!-- /form -->
+        </div>
+        <!-- /.card -->
+
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  @endsection
+
+  @section('script')
+    <script src="{{URL::asset('extadmin/plugins/summernote/summernote-bs4.min.js')}}"></script>
+    <script>
+    $(function () {
+        // Summernote
+        $('.textarea').summernote()
+    })
+    </script>
+  @endsection
